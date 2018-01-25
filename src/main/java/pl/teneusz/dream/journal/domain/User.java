7f750d87f7639331d983df6a13d32cc7a761fad5
Teneusz <teneusz@wp.pl>
2018-01-25 20:37:53 +0100

    commit

--- src/main/java/pl/teneusz/dream/journal/domain/User.java
+++ src/main/java/pl/teneusz/dream/journal/domain/User.java
@@ -97,7 +97,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @BatchSize(size = 20)
     private Set<Authority> authorities = new HashSet<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     @JsonIgnore
     @JoinColumn(name = "id")
     private UserDetails userDetails;
